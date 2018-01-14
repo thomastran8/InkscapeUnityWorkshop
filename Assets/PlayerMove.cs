b33ff555450ac6c1b60eade189fcf0c0e4fc9810
Thomas <thomastran878@gmail.com>
2018-01-13 22:41:38 -0800

    Added Project and svg file

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate () {
+        rb.velocity = new Vector2(0, rb.velocity.y) + Vector2.right * moveSpeed;
+	}
+}
